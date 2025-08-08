66acbe3f8da316b2bced4f32b0207befdd1e08d1
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-08 22:36:39 +0300

    Do while

--- index.js
+++ index.js
@@ -4,8 +4,15 @@
 
 // The same
 
-let i = 0;
-while (i <= 5) {
+// let i = 0;
+// while (i <= 5) {
+//   if (i % 2 !== 0) console.log(i);
+//   i++;
+// }
+
+// do-while διαφορα εκτελειται τουλαχιστον μια φορα
+let i = 1;
+do {
   if (i % 2 !== 0) console.log(i);
   i++;
-}
+} while (i <= 5);
