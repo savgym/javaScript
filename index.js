b32ff1c5b253601fce13fa5eef1a8e007de33efb
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-08 22:24:36 +0300

    For Loop

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-let role = "guest";
+for (let i = 0; i < 5; i++) {
+  console.log("Hello World", i);
+}
 
-switch (role) {
-  case "guest":
-    console.log("Guest User");
-    break;
-  case "moderator":
-    console.log("Moderator User");
-    breal;
-  default:
-    console.log("Unknown User");
+for (let i = 1; i <= 5; i++) {
+  if (i % 2 !== 0) console.log(i);
+}
+
+for (let i = 5; i >= 1; i--) {
+  if (i % 2 !== 0) console.log(i);
 }
