b1c1bc3a87e979b2462b23e99488b83788df5b4e
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-09 05:52:13 +0300

    FuzzBuzz

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function isLandscape(width, height) {
-  return width > height;
-}
+// Divisible by 3 => Fuzz
+// Divisible by 5 => Buzz
+// Divisible by 3 and 5 => FuzzBuzz
+// Not divisible by 3 and 5 => input
+// Not a number =>'Not a number'
 
-console.log(isLandscape(800, 300));
+const output = fizzBuzz(false);
+console.log(output);
+
+function fizzBuzz(input) {
+  if (typeof input !== "number") return NaN;
+  if (input % 3 === 0 && input % 5 === 0) return "FuzzBuzz";
+  if (input % 5 === 0) return "Buzz";
+  if (input % 3 === 0) return "Fuzz";
+  return input;
+}
