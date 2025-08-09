be7b7e040b8262f6e96a32a0eabcb74ed9cc9a5b
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-09 21:11:41 +0300

    Exersise 6 Count Truthy

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-showNumber(10);
+// Falsy
+// undefined
+// null
+// ''
+// false
+// 0
+// NaN
+const array = [0, null, undefined, "", 2, 3];
 
-function showNumber(limit) {
-  for (let i = 0; i <= limit; i++) {
-    const message = i % 2 === 0 ? "ODD" : "EVEN";
-    console.log(i, message);
-  }
+console.log(countTruthy(array));
+
+function countTruthy(array) {
+  let count = 0;
+  for (let value of array) if (value) count++;
+  return count;
 }
