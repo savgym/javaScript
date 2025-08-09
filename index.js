9722b7f4d813e5bf5d8873201ef4229b70860461
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-09 05:25:28 +0300

    Exercise Max , output true ,false

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-let number = max(2, 1);
-console.log(number);
-
-// function max(a, b) {
-//   if (a > b) return a;
-//   else return b;
-// }
-// function max(a, b) {
-//   if (a > b) return a;
-//   return b;
-// }
-function max(a, b) {
-  return a > b ? a : b;
+function isLandscape(width, height) {
+  return width > height;
 }
+
+console.log(isLandscape(800, 300));
