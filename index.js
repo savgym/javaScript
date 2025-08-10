6d0cdc310edd3570b6e9383b2e355e5d4e29b9dd
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-10 18:59:55 +0300

    Factory Function

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-//  Object-oriented Programming (OOP)
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  isVisible: true,
-  draw: function () {
-    console.log("draw");
-  },
-};
+// Factory Function
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log("draw");
+    },
+  };
+}
 
-circle.draw(); // Method
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
