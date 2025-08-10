c64bde32abcbb6b07da274723f5e9d5789b35a54
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-10 19:46:33 +0300

    Function are Objects

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-// Factory Function
-function createCircle(radius) {
-  return {
-    radius,
-    draw() {
-      console.log("draw");
-    },
-  };
-}
-
-const circle = createCircle(1);
-
-// Constraction Function
 function Circle(radius) {
   this.radius = radius;
   this.draw = function () {
     console.log("drow");
   };
 }
-
+Circle.call({}, 1);
+Circle.apply({}, [1]); // an exeis array
 const another = new Circle(1);
-
-new String(); // '',""
-new Boolean(); //true ,false
-new Number(); //1,2,3
