71d8d824f2d3777f6212e940459c3c8189d9bae7
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-11 11:06:01 +0300

    Exercise 3 Object Equality

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-// street
-// city
-// zipCode
-// showAddress(address)
+//Constuctor Function
 
-const address = {
-  street: "dodekanisou",
-  city: "Athens",
-  zipCode: 14235,
-};
-
-// Factory Function
-function showAddress(address) {
-  return {
-    street,
-    city,
-    zipCode,
-  };
+function Address(street, city, zipCode) {
+  this.street = street;
+  this.city = city;
+  this.zipCode = zipCode;
 }
 
-showAddress(address);
-//Constaction Function
+let address1 = new Address("a", "b", "c");
+let address2 = new Address("a", "b", "c");
+let address3 = address1;
 
-function ShowAddress(address){
-  this.street=
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+console.log(areSame(address1, address3));
+
+function areSame(address1, address2) {
+  return address1 === address2;
+}
+
+function areEqual(address1, address2) {
+  return (
+    address1.street === address2.street &&
+    address1.city === address2.city &&
+    address1.zipCode === address2.zipCode
+  );
 }
