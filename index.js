ee69bf47268778b547a080ff73aa5897ad921054
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-11 13:01:53 +0300

    Exercise 5 Constuctor Function

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const post = {
-  title: "a",
-  body: "b",
-  author: "c",
-  views: 12,
-  comments: [
-    { author: "c", body: "d" },
-    { author: "c", body: "d" },
-  ],
-  isLive: true,
-};
-
+let post = new Post("a", "b", "c");
 console.log(post);
+
+function Post(title, body, author) {
+  this.title = title;
+  this.body = body;
+  this.author = author;
+  this.views = 0;
+  this.comments = [];
+  this.isLive = false;
+}
