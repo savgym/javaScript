b108c4a55d7d551215fe865439019ea885a6a7b7
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-11 15:55:36 +0300

    Arrays Adding Elements

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let priceRanges = [
-  { label: "$", tooltip: "Inexpensive", minPerPerson: 0, maxPerPerson: 10 },
-  { label: "$$", tooltip: "Moderate", minPerPerson: 11, maxPerPerson: 20 },
-  { label: "$$$", tooltip: "Expensive", minPerPerson: 21, maxPerPerson: 50 },
-];
+const numbers = [3, 4];
 
-let restaurants = [{ averagePerPerson: 5 }];
+// End
+numbers.push(5, 6);
+
+// Beginning;
+numbers.unshift(1, 2);
+
+// Middle.
+numbers.splice(2, 0, "a", "b");
+
+console.log(numbers);
