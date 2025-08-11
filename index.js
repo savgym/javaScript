ee5e9e59ac24fdfeba29091eb50c7eaa37a3f0da
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-11 12:37:02 +0300

    Exercise 4 Blog Post Object

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-//Constuctor Function
+const post = {
+  title: "a",
+  body: "b",
+  author: "c",
+  views: 12,
+  comments: [
+    { author: "c", body: "d" },
+    { author: "c", body: "d" },
+  ],
+  isLive: true,
+};
 
-function Address(street, city, zipCode) {
-  this.street = street;
-  this.city = city;
-  this.zipCode = zipCode;
-}
-
-let address1 = new Address("a", "b", "c");
-let address2 = new Address("a", "b", "c");
-let address3 = address1;
-
-console.log(areEqual(address1, address2));
-console.log(areSame(address1, address2));
-console.log(areSame(address1, address3));
-
-function areSame(address1, address2) {
-  return address1 === address2;
-}
-
-function areEqual(address1, address2) {
-  return (
-    address1.street === address2.street &&
-    address1.city === address2.city &&
-    address1.zipCode === address2.zipCode
-  );
-}
+console.log(post);
