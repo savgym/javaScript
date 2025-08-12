693dfd338c532865e48407073e33fcfaacddb2b4
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-12 22:13:53 +0300

    Exercise 7- Movies

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const numbers = [1, 2, 3, 4];
+const movies = [
+  { title: "a", year: 2018, rating: 4.5 },
+  { title: "b", year: 2018, rating: 4.7 },
+  { title: "c", year: 2018, rating: 3 },
+  { title: "d", year: 2017, rating: 4.5 },
+];
 
-const max = getMax(numbers);
+// All the movies in 2018 with rating >4
+// Sort them by their rating
+// Descending order
+// Pick their title
+// 'b','a'
 
-console.log(max);
+const titles = movies
+  .filter((m) => m.year === 2018 && m.rating >= 4)
+  .sort((a, b) => a.rating - b.rating)
+  .reverse()
+  .map((m) => m.title);
 
-// function getMax(array) {
-//   if (array.length === 0) return undefined;
-
-//   let max = array[0];
-
-//   for (let i = 1; i < array.length; i++) if (array[i] > max) max = array[i];
-
-//   return max;
-// }
-
-function getMax(array) {
-  if (array.length === 0) return undefined;
-
-  return array.reduce((a, b) => (a > b ? a : b));
-}
+console.log(titles);
