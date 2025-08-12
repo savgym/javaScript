8561818a29c18f2ccd2e3d78a002886bc14253e6
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-12 08:02:19 +0300

    Exercise 1-Array from Range

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const numbers = [1, -1, 2, 3];
+const numbers = arrayFromRange(1, 4);
 
-//a = 0 , c=1 => a =1
-//a = 1 , c= -1 => a =0
-//a = 0 , c=2 => a =2
-//a = 2 , c=3 => a =5
+console.log(numbers);
 
-const sum = numbers.reduce(
-  (accummulator, currentValue) => accummulator + currentValue,
-  0
-);
-
-console.log(sum);
+function arrayFromRange(min, max) {
+  let output = [];
+  for (let i = min; i <= max; i++) output.push(i);
+  return output;
+}
