80ca817feee84bdc8274c0f61dcbff313fe9cbe7
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-12 03:06:11 +0300

    Mapping an Array

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const numbers = [1, -1, 2, 3];
 
 const filtered = numbers.filter((n) => n >= 0);
 
+const items = filtered.map((n) => "<li>" + n + "<li>");
+
+const html = "<ul>" + items.join("") + "<ul>";
+
 console.log(filtered);
+console.log(items);
+console.log(html);
+
+const items2 = numbers.filter((n) => n >= 0).map((n) => ({ value: n }));
+
+console.log(items2);
