37bbe9fe927316e803f62b0a977f58905aa2e7b1
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-13 08:21:16 +0300

    This keyword

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-const person = {
-  firstName: "Mosh",
-  lastName: "Hamedani",
-  get fullName() {
-    return `${person.firstName} ${person.lastName}`;
-  },
-  set fullName(value) {
-    if (typeof value !== "string") throw new Error("Value is not a string.");
+// method -> obj
+// function -> global (window, global)
 
-    const parts = value.split(" ");
-    if (parts.length !== 2) throw new Error("Enter a first and last name.");
-    this.firstName = parts[0];
-    this.lastName = parts[1];
+const video = {
+  title: "a",
+  tags: ["a", "b", "c"],
+  showTags() {
+    this.tags.forEach(function (tag) {
+      console.log(this.title, tag);
+    }, this);
   },
 };
 
-try {
-  person.fullName = "";
-} catch (e) {
-  alert(e);
-}
-
-console.log(person);
+video.showTags();
