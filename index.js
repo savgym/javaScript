1ff50d7aa27dd57f0be4acc5fdf22d09ad240a0a
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-13 10:57:28 +0300

    Exercise 2 -Area of Circle

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// sum([1,2,3,4]) => 10
-
-function sum(...items) {
-  if (items.length === 1 && Array.isArray(items[0])) items = [...items[0]];
-  return items.reduce((a, b) => a + b);
-}
-
-console.log(sum([1, 2, 3, 4]));
+let circle = {
+  radius: 2,
+  area: 3,
+};
