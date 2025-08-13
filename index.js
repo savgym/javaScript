580036e5171f13b6c34459edb42eb2c7d0f10b5b
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-13 07:48:17 +0300

    Try and Catch

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ const person = {
     return `${person.firstName} ${person.lastName}`;
   },
   set fullName(value) {
+    if (typeof value !== "string") throw new Error("Value is not a string.");
+
     const parts = value.split(" ");
+    if (parts.length !== 2) throw new Error("Enter a first and last name.");
     this.firstName = parts[0];
     this.lastName = parts[1];
   },
 };
 
-person.fullName = "John Smith";
-// getters => access properties
-// setters => change (mutate) them
+try {
+  person.fullName = "";
+} catch (e) {
+  alert(e);
+}
 
 console.log(person);
