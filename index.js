70d643c418c7eadac27c64d245906da2e0cf8bea
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-13 11:12:48 +0300

    Exercise 3-Error Handiling

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-let circle = {
-  radius: 2,
-  area: 3,
-};
+try {
+  const numbers = 5;
+  const count = countOccurrences(numbers, 1);
+  console.log(count);
+} catch (e) {
+  console.log(e.message);
+}
+
+function countOccurrences(array, searchElement) {
+  if (!Array.isArray(array)) throw new Error("Invalid Array");
+
+  return array.reduce((accumulator, current) => {
+    const occurrence = current === searchElement ? 1 : 0;
+    return accumulator + occurrence;
+  }, 0);
+}
