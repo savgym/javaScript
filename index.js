23d3f9c1dad38b0d91f889784d6f3244a4602fe0
Savvas Gymnopoulos <sav_tph@hotmail.com>
2025-08-13 07:14:37 +0300

    Rest Operator

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function sum() {
-  let total = 0;
-  for (let value of arguments) total += value;
-  return total;
+// function sum(...args) {
+//   return args.reduce((a, b) => a + b);
+// }
+function sum(discount, ...prices) {
+  const total = prices.reduce((a, b) => a + b);
+  return total * (1 - discount);
 }
 
-console.log(sum(1, 2, 3, 4, 5, 6));
+console.log(sum(0.1, 20, 30));
